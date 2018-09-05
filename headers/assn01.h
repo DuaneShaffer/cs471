10e40dca7b74052a900f0b0ec4a6c6f2b3ec3ff4
DuaneShaffer <dlshaffer@alaska.edu>
2018-09-05 10:37:53 -0800

    Templates can't be used in .cpp.. Made appropriate changes

---
+++ headers/assn01.h
@@ -0,0 +1,22 @@
+// assn01.h
+// Duane Shaffer
+// 6 September 2018
+//
+// For CS 471 Fall 2018
+// Header file for quicksort algorithm
+// Used in Assignment 1
+// Requires assn01.cpp
+
+#include <vector> 
+
+
+template <typename container, typename RAIter>
+void my_quicksort(container & input, RAIter begin, RAIter end){
+}
+
+template <typename container, typename RAIter>
+RAIter my_partition(container & input, RAIter begin, RAIter end){
+    return begin;
+}
+
+
